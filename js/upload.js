468fc7b0ad5443dd36725aa66a740a4e5adfbbeb
Luan Neves <luanpneves@gmail.com>
2024-06-28 16:00:07 -0300

    finalização do arquivo upload de fotos e editar foto.

--- js/upload.js
+++ js/upload.js
@@ -2,11 +2,49 @@ $(document).ready(function () {
     var mensagem = $("#mensagem");
     var preloader = $("#preloader");
     var barra = $("#barra");
-    $("#editarFoto").hide();
-   
+    
+    // Esconder elementos inicialmente
+    mensagem.hide();
+    preloader.hide();
 
-    $("#bnt_editar_foto").on('click', function () {
-        $("#editarFoto").toggle();
+    $("#btn-editar-foto").on('click', function () {
+        $("#editar-foto").toggle();
+    });
 
+    $("#btn-enviar-foto").on('click', function (event) {
+        event.preventDefault();
+        
+        // Usando ajaxForm para configurar o envio de arquivo
+        $("#form-upload-foto").ajaxForm({
+            url: './paginas/contatos/upload/executa-upload.php',
+            uploadProgress: function (event, position, total, percentComplete) {
+                // Exibindo preloader e atualizando a barra de progresso
+                preloader.show();
+                barra.width(percentComplete + '%');
+                barra.html(percentComplete + '%');
+            },
+            success: function (data) {
+                // Processamento do retorno do servidor após o envio
+                var msg = data.substring(0, data.indexOf(';'));
+                var tipoMsg = data.substring(data.indexOf(';') + 1);
+
+                if (tipoMsg == "concluido") {
+                    var caminho_foto = msg;
+                    msg = "Upload da foto realizado com sucesso!";
+                    $("#foto-contato").attr("src", caminho_foto + "?timestamp=" + new Date().getTime());
+                    mensagem.show().attr("class", "mb-3 alert alert-success").html(msg);
+                } else if (tipoMsg == "aviso") {
+                    mensagem.show().attr("class", "mb-3 alert alert-warning").html(msg);
+                    preloader.hide();
+                } else {
+                    mensagem.show().attr("class", "mb-3 alert alert-danger").html(msg);
+                    preloader.hide();
+                }
+            },
+            error: function (data) {
+                // Tratamento de erro durante o envio
+                console.log(data);
+            }
+        }).submit();
     });
 });
--- paginas/contatos/editarContato.php
+++ paginas/contatos/editarContato.php
@@ -9,10 +9,9 @@ $dados = mysqli_fetch_assoc($rs);
 </header>
 
 <div class="row">
-
-    <div class=" col-6">
+    <div class="col-6">
         <form class="needs-validation" action="index.php?menuop=atualizarContato" method="post" novalidate>
-            <div class="mb-3 ">
+            <div class="mb-3">
                 <label class="form-label" for="idContato">ID</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="bi bi-key"></i></span>
@@ -20,36 +19,35 @@ $dados = mysqli_fetch_assoc($rs);
                 </div>
             </div>
 
-            <div class="mb-3 ">
+            <div class="mb-3">
                 <label class="form-label" for="nomeContato">Nome</label>
                 <div class="input-group">
-                    <span class="input-group-text"><i class="bi bi-person-fill"> </i></span>
+                    <span class="input-group-text"><i class="bi bi-person-fill"></i></span>
                     <input class="form-control" type="text" name="nomeContato" value="<?= $dados["nomeContato"] ?>" required>
-                    <div class="valid-feedback color"> Tudo certo!</div>
+                    <div class="valid-feedback color">Tudo certo!</div>
                     <div class="invalid-feedback">Campo obrigatório</div>
                 </div>
             </div>
 
-            <div class="mb-3 ">
+            <div class="mb-3">
                 <label class="form-label" for="emailContato">E-mail</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="bi bi-envelope-at"></i></span>
                     <input class="form-control" type="email" name="emailContato" value="<?= $dados["emailContato"] ?>" required>
-                    <div class="valid-feedback color"> Tudo certo!</div>
+                    <div class="valid-feedback color">Tudo certo!</div>
                     <div class="invalid-feedback">Campo obrigatório</div>
                 </div>
             </div>
 
-            <div class="mb-3 ">
+            <div class="mb-3">
                 <label class="form-label" for="telefoneContato">Telefone</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="bi bi-telephone"></i></span>
                     <input class="form-control" type="text" name="telefoneContato" value="<?= $dados["telefoneContato"] ?>" required>
-
                 </div>
             </div>
 
-            <div class="mb-3 ">
+            <div class="mb-3">
                 <label class="form-label" for="enderecoContato">Endereço</label>
                 <div class="input-group">
                     <span class="input-group-text"><i class="bi bi-mailbox2"></i></span>
@@ -62,73 +60,63 @@ $dados = mysqli_fetch_assoc($rs);
                     <label class="form-label" for="sexoContato">Gênero</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="bi bi-gender-ambiguous"></i></span>
-                        <select class="form-control" type="text" name="sexoContato" value="<?= $dados["sexoContato"] ?>" required>
-                            <option <?php echo ($dados['sexoContato'] == '') ? 'selected' : '' ?> value="">Selecione o seu Gênero. </option>
+                        <select class="form-control" name="sexoContato" required>
+                            <option <?php echo ($dados['sexoContato'] == '') ? 'selected' : '' ?> value="">Selecione o seu Gênero.</option>
                             <option <?php echo ($dados['sexoContato'] == 'M') ? 'selected' : '' ?> value="M">Masculino</option>
                             <option <?php echo ($dados['sexoContato'] == 'F') ? 'selected' : '' ?> value="F">Feminino</option>
-                            <option <?php echo ($dados['sexoContato'] == 'O') ? 'selected' : '' ?> value="O">Outros</option>>
+                            <option <?php echo ($dados['sexoContato'] == 'O') ? 'selected' : '' ?> value="O">Outros</option>
                         </select>
                     </div>
                 </div>
 
                 <div class="mb-3 col-6">
-                    <label class="form-label" for="dataNascimentoContato">Data de Nascimento</label required>
+                    <label class="form-label" for="dataNascimentoContato">Data de Nascimento</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="bi bi-calendar-date"></i></span>
-                        <input class="input-group-text" type="date" name="dataNascimentoContato" value="<?= $dados["dataNascimentoContato"] ?>">
+                        <input class="form-control" type="date" name="dataNascimentoContato" value="<?= $dados["dataNascimentoContato"] ?>" required>
                     </div>
                 </div>
-
-
             </div>
 
-
             <div class="mb-3">
-                <input class="btn btn-warning mb-3 class=" form-control type="submit" value="Atualizar" name="btnAtualizar">
+                <input class="btn btn-warning mb-3 form-control" type="submit" value="Atualizar" name="btnAtualizar">
             </div>
         </form>
     </div>
 
     <div class="col-6">
         <?php
-        if ($dados["nomeFotoContato"] == "" || !file_exists('./paginas/contatos/fotos-contato/' . $dados["nomeFotoContato"])) {
+        if ($dados["nomeFotoContato"] == "" || !file_exists('./paginas/contatos/fotos-contatos/' . $dados["nomeFotoContato"])) {
             $nomeFoto = "Semavatar.png";
         } else {
             $nomeFoto = $dados["nomeFotoContato"];
         }
-
-
         ?>
-        <div class="col-6 img-fluid "> <img width="500" class=" rounded-circle " src="./paginas/contatos/fotos-contato/<?= $nomeFoto ?>" alt="Foto Contato">
+        <div class="col-6 img-fluid">
+            <img id="foto-contato" width="300" class="rounded-circle mb-3" src="./paginas/contatos/fotos-contatos/<?= $nomeFoto ?>" alt="Foto Contato">
         </div>
 
         <div class="mb-3">
-            <button class="btn btn-warning" id="bnt_editar_foto"><i class="bi bi-camera2"> Editar Foto</i></button>
+            <button class="btn btn-warning" id="btn-editar-foto"><i class="bi bi-camera2"> Editar Foto</i></button>
         </div>
 
-        <div id="editarFoto">
-
+        <div id="editar-foto">
             <form id="form-upload-foto" class="mb-3" action="" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="idContato" value="<?= $idContato ?>">
                 <label class="form-label" for="arquivo">Selecione um arquivo</label>
 
                 <div class="input-group">
-
                     <input class="form-control" type="file" name="arquivo" id="arquivo">
-                    <input class="btn btn-warning" type="submit" value="Enviar">
-
+                    <input id="btn-enviar-foto" class="btn btn-warning" type="submit" value="Enviar">
                 </div>
-
             </form>
-            <div id="mensagem" class=" mb-3 alert alert-success">
+            <div id="mensagem" class="mb-3 alert alert-success">
                 Carregando
             </div>
 
             <div id="preloader" class="progress" role="progressbar" aria-label="Success example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
                 <div id="barra" class="progress-bar bg-warning" style="width: 0%">0%</div>
             </div>
-
         </div>
-
     </div>
 </div>
